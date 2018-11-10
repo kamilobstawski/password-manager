b1ef38f9adc3b95c8c6bcae4ddbd001372e6365b
Kamil Obstawski <kamil.obstawski@itconfig.com.pl>
2018-11-10 19:08:05 +0100

    Added functionality to share entry with special token

--- entries/urls.py
+++ entries/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'create-entry/$', views.CreateEntryView.as_view(), name='create_entry'),
     url(r'update-entry/(?P<pk>[0-9]+)/$', views.UpdateEntryView.as_view(), name='update_entry'),
     url(r'delete-entry/(?P<pk>[0-9]+)/$', views.DeleteEntryView.as_view(), name='delete_entry'),
+    url(r'generate-link/(?P<entry_pk>[0-9]+)/$', views.GenerateLink.as_view(), name='generate_link'),
+    url(r'temporary-entry/(?P<token>\w+)/$', views.TemporaryEntryView.as_view(), name='temporary_entry'),
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,12 +1,20 @@
+import json
+
+from django.forms import model_to_dict
+from django.shortcuts import get_object_or_404
+from django.core import serializers
 from rest_framework.generics import (ListAPIView,
                                      CreateAPIView,
                                      DestroyAPIView,
                                      UpdateAPIView,
                                      RetrieveAPIView)
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import EntrySerializer
 from .models import Entry
+from . import service
 
 
 class EntryView(RetrieveAPIView):
@@ -41,3 +49,21 @@ class DeleteEntryView(DestroyAPIView):
     permission_classes = (AllowAny,)
     serializer_class = EntrySerializer
     queryset = Entry.objects.all()
+
+
+class GenerateLink(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, entry_pk):
+        link = service.generate_link(entry_pk, request)
+        return Response({'link': link})
+
+
+class TemporaryEntryView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, token):
+        entry_pk = service.check_token(token)
+        entry = get_object_or_404(Entry, pk=entry_pk)
+        dict_obj = model_to_dict(entry)
+        return Response(dict_obj)
