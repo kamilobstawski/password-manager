d251a05267cfb90f0602a2f6cc3180e57f05314e
Kamil Obstawski <kamil.obstawski@itconfig.com.pl>
2018-11-11 19:24:41 +0100

    Added jwt login

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', obtain_jwt_token, name='login'),
     url(r'', include('entries.urls', namespace='entries')),
 ]
